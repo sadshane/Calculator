19e2630ab9230ca9e5ed2d0f926bd03b90641417
Shane <71f4ldre@duck.com>
2023-08-08 12:58:53 +0800

    added functionality to get input but still no operation

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log('Shania Claire');
+
+function getInputs() {
+    let keys = document.querySelectorAll('.key')
+    let input = document.querySelector('.answer');
+    keys.forEach(key => {
+        key.addEventListener('click', () => {
+            checkInput(input, key);
+        });
+    })
+}
+
+function checkInput(input, key) {
+    if (key.textContent === 'AC')
+    {
+        input.textContent = 0;
+        return;
+    }
+    if (!Number.isInteger(+key.textContent))
+    {
+        console.log("not ints");
+    }
+    if (key.textContent === 'Del' && input.textContent.length === 1)
+    {
+        input.textContent = 0;
+    }
+    else if (input.textContent === '0' && input.textContent.length === 1)
+    {
+        input.textContent = key.textContent;
+    }
+    else if (key.textContent === 'Del')
+    {
+        input.textContent = input.textContent.slice(0, -1);
+    }
+    else 
+    {
+        input.textContent = input.textContent + key.textContent;
+    }
+}
+
+function operate() {
+
+}
+
+getInputs();
